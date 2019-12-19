7aa5c7ba83a39616ba542fed944c60f552dfafac
seyio91 <seyi_obaweya@yahoo.com>
2019-12-19 02:06:26 +0200

    added sqlalchemy pagination

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'pa55wor0'
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
         SQLALCHEMY_TRACK_MODIFICATIONS = False
+        POSTS_PER_PAGE = 3
