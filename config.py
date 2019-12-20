04f47a1b3be51ab818cb19d7987c1a994436b014
seyio91 <seyi_obaweya@yahoo.com>
2019-12-20 03:28:25 +0200

    added email logging support

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -6,3 +9,10 @@ class Config(object):
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
         SQLALCHEMY_TRACK_MODIFICATIONS = False
         POSTS_PER_PAGE = 3
+        MAIL_SERVER = os.environ.get('MAIL_SERVER')
+        MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+        MAIL_USE_TLS = False
+        MAIL_USE_SSL = True
+        MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+        ADMINS = ['seyi_obaweya@yahoo.com']
